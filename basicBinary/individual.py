6f84a041204ce62802e491628e26b5b41d89eec0
Allison Kaptur <allison.kaptur@gmail.com>
2013-11-04 13:42:33 -0500

    clean up individual init

--- basicBinary/individual.py
+++ basicBinary/individual.py
@@ -6,15 +6,12 @@ class Individual(object):
     def __init__(self):
         self._default_gene_length = 64
         self._fitness = 0
-        self.genes = BitArray()
-        
-        for i in range(0, self._default_gene_length):
-            self.genes.bin += str('0')
-        
+        self.genes = BitArray(bin="0" * self._default_gene_length)
+
     def generate_individual(self):
         genes = [int(round(random())) for i in range(self._default_gene_length)]
         self.genes = BitArray(genes)
-    
+
     def get_fitness(self, solution):
         if self._fitness == 0:
             pairs = zip(self.genes, solution)
